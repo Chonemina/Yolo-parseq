35a5d7cfcf7d1e6c9ae2e629f1d5603eb1e02bf1
Chonemina <ducnguyendz135@gmail.com>
2024-03-31 13:18:45 +0700

    sua

--- autoCorrect.py
+++ autoCorrect.py
@@ -1,6 +1,7 @@
 #chua cac ham co the hoan thanh tac vu chinh su nhung class con thieu
 #ham tim xem quest_id nao con thieu
-Example=["câu 51","câu 52","câu 54"]
+Example=["câu51.","câu52.","câu 55."]
+"""
 def lostquest_id(input_list):
     
     input_string= ",".join(input_list)
@@ -18,5 +19,24 @@ def lostquest_id(input_list):
         print ("Khong con thieu cau hoi nao")
     else:
         print("Các quest_id còn thiếu:", missing_number)
- 
-#lostquest_id(Example)
+    return missing_number
+#a=lostquest_id(Example)
+#print(a)
+"""
+def is_inside(rect, rectlist):
+    
+    for r in rectlist:
+        [x1,y1,x2,y2]=r
+        if x1 <= rect[0] <= x2 and y1 <= rect[1] <= y2:
+            return True
+    return False
+   
+    
+
+rect_list = [[0, 0, 5, 6], [2, 2, 3, 6], [10, 10, 44, 15]]
+rect = [3, 3, 8, 4]  # Tọa độ của rect (góc trên bên trái: (3, 3), góc dưới bên phải: (4, 4))
+
+#if is_inside(rect, rect_list):
+ #   print("rect nằm hoàn toàn bên trong các hìn trong rect_list.")
+#else:
+  #  print("rect không nằm hoàn toàn bên trong các hìnhtrong rect_list.")
